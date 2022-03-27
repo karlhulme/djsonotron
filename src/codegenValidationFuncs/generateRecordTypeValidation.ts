62de982ffa881cef4e9070364e70bb46a4640108
karlhulme <karlhulme@hotmail.com>
2022-03-27 09:17:11 +0100

    Fixed bug in valueDisplayPath of record type valiation.

--- src/codegenValidationFuncs/generateRecordTypeValidation.ts
+++ src/codegenValidationFuncs/generateRecordTypeValidation.ts
@@ -135,7 +135,7 @@ export function generateRecordTypeValidation(props: RecordTypeValidationProps) {
           ${
         generateRecordTypePropertyValidation({
           valuePath: `${props.valuePath}.${property.name}`,
-          valueDisplayPath: props.valueDisplayPath,
+          valueDisplayPath: `${props.valueDisplayPath}.${property.name}`,
           def: propertyValueTypeDef,
           types: props.types,
         })
