79bac7a88b3c9bf4beb525078b3ed7ecc1669618
karlhulme <karlhulme@hotmail.com>
2022-06-03 10:40:40 +0100

    Added utility functions for sengi.

---
+++ src/forSengi/generateSengiUtilityCode.ts
@@ -0,0 +1,39 @@
+/**
+ * Returns utility types, errors and functions that are used for
+ * bridging jsonotron and sengi.
+ */
+export function generateSengiUtilityCode() {
+  return `
+  /**
+   * Raised when the api key is missing.
+   */
+  export class ServiceApiKeyMissingError extends Error {
+    constructor() {
+      super("Missing x-api-key header.");
+      Object.setPrototypeOf(this, new.target.prototype);
+      this.name = this.constructor.name;
+    }
+  }
+
+  /**
+   * Raises an error if the given value is not a non-zero length string.
+   */
+  function ensureApiKeyHeaderValue (value: string|null): string {
+    if (typeof value === "string" && value.length > 0) {
+      return value
+    } else {
+      throw new ServiceApiKeyMissingError();
+    }
+  }
+  
+  /**
+   * The options that can be passed to the createSengiStandardOperations function.
+   */
+  interface CreateSengiStandardOperationsOptions {
+    /**
+     * The partition to use when a partition is not supplied with a request.
+     */  
+    defaultPartition: string
+  }  
+  `;
+}
--- src/forSengi/index.ts
+++ src/forSengi/index.ts
@@ -1,4 +1,5 @@
 export * from "./generateSengiDocTypeInputOutputVariants.ts";
 export * from "./generateSengiServicePaths.ts";
 export * from "./generateSengiServiceSignatures.ts";
+export * from "./generateSengiUtilityCode.ts";
 export * from "./SengiSeedDocType.ts";
