1de5989e3e5f43355459637ba395d8006489d518
karlhulme <karlhulme@hotmail.com>
2022-07-10 09:12:01 +0100

    Allow multiple user claims per resource.

--- src/std/stringTypes/userClaim.ts
+++ src/std/stringTypes/userClaim.ts
@@ -8,8 +8,10 @@ export const userClaim: StringTypeDef = {
   maximumLength: 80,
   summary: `A user claim in the form resourceType:resourceId:role.`,
   regex:
-    "^[a-zA-Z]+:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}:[a-zA-Z]+$",
+    "^[a-zA-Z]+:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}:[a-zA-Z]+(,[a-zA-Z]+)*$",
   validTestCases: [{
     value: "club:00000000-0000-0000-0000-000000000000:admin",
+  }, {
+    value: "club:00000000-0000-0000-0000-000000000000:admin,support",
   }],
 };
--- src/std/stringTypes/userClaimsString.ts
+++ src/std/stringTypes/userClaimsString.ts
@@ -2,17 +2,17 @@ import { StringTypeDef } from "../../interfaces/index.ts";
 import { stdSystemName } from "../stdSystemName.ts";
 
 const userClaimRegex =
-  "[a-zA-Z]+:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}:[a-zA-Z]+";
+  "[a-zA-Z]+:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}:[a-zA-Z]+(,[a-zA-Z]+)*";
 
 export const userClaimsString: StringTypeDef = {
   kind: "string",
   system: stdSystemName,
   name: "userClaim",
-  maximumLength: 2000,
+  maximumLength: 1000,
   summary: "A user claim in the form resourceType:resourceId:role.",
   regex: `^${userClaimRegex}( ${userClaimRegex})*$`,
   validTestCases: [{
     value:
-      "club:00000000-0000-0000-0000-000000000000:support club:00000000-0000-0000-0000-000000000000:marketeer",
+      "club:00000000-0000-0000-0000-000000000000:support club:00000000-0000-0000-0000-000000000001:marketeer,designer",
   }],
 };
