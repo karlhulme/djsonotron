08225da74a2d0cedbb1f8f3409c3fa243489d413
karlhulme <karlhulme@hotmail.com>
2022-09-25 14:47:15 +0100

    Fixed formatDateTimeUtc parameter comment.

--- src/forRuntime/formatDateTimeUtc.ts
+++ src/forRuntime/formatDateTimeUtc.ts
@@ -2,7 +2,7 @@ import { formatDate } from "../../deps.ts";
 
 /**
  * Returns a UTC date time string for the given date object.
- * @param date A ate.
+ * @param date A date.
  */
 export function formatDateTimeUtc(date: Date) {
   return formatDate(date, "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
