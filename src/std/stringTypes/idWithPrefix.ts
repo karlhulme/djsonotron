ce9633cb98165a4d26baa6d4d16e7966425d52a2
karlhulme <karlhulme@hotmail.com>
2022-09-26 22:03:15 +0100

    relax idWithPrefix to allow for test and system user accounts.

--- src/std/stringTypes/idWithPrefix.ts
+++ src/std/stringTypes/idWithPrefix.ts
@@ -10,10 +10,14 @@ export const idWithPrefix: StringTypeDef = {
     `An id with a prefix of 2 to 5 letters that identifies the type of object.  The subsequent
      characters should include an encoded date-time component so that codes
      can be stored sequentially and a random section.`,
-  regex: "^[a-z]{2,5}_[a-zA-Z0-9]{8,44}$",
+  regex: "^[a-z]{2,5}_[a-zA-Z0-9]{4,44}$",
   maximumLength: 50,
   validTestCases: [{
     value: "ex_234g23jh534hgk35jhkjhk",
+  }, {
+    value: "ex_test",
+  }, {
+    value: "ex_system",
   }],
   invalidTestCases: [{
     value: "ex234g23jh534hgk35jhkjhk",
