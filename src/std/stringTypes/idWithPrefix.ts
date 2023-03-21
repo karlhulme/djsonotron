44881e130bf2bf198a14e5120852ce4e78592699
Karl Hulme <karlhulme@hotmail.com>
2023-03-21 11:05:13 +0000

    idWithPrefix type extended to allow 6 character prefixes.

--- src/std/stringTypes/idWithPrefix.ts
+++ src/std/stringTypes/idWithPrefix.ts
@@ -7,21 +7,21 @@ export const idWithPrefix: StringTypeDef = {
   name: "idWithPrefix",
   pluralName: "idsWithPrefix",
   summary:
-    `An id with a prefix of 2 to 5 letters that identifies the type of object.  The subsequent
+    `An id with a prefix of 2 to 6 letters that identifies the type of object.  The subsequent
      characters should include an encoded date-time component so that codes
      can be stored sequentially and a random section.`,
-  regex: "^[a-z]{2,5}_[a-zA-Z0-9]{4,44}$",
+  regex: "^[a-z]{2,6}_[a-zA-Z0-9]{4,44}$",
   maximumLength: 50,
   validTestCases: [{
     value: "ex_234g23jh534hgk35jhkjhk",
   }, {
     value: "ex_test",
   }, {
-    value: "ex_system",
+    value: "exampl_system",
   }],
   invalidTestCases: [{
     value: "ex234g23jh534hgk35jhkjhk",
   }, {
-    value: "ex_234g23jh534hgk_35jhkjhk",
+    value: "toolong_234g23jh534hgk_35jhkjhk",
   }],
 };
