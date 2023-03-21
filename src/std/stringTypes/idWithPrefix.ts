8559a46aa7d7205acfb1676a950dc6150be74b7d
Karl Hulme <karlhulme@hotmail.com>
2023-03-21 15:58:56 +0000

    Id prefix no longer accepts initial capital letter.

--- src/std/stringTypes/idWithPrefix.ts
+++ src/std/stringTypes/idWithPrefix.ts
@@ -7,27 +7,24 @@ export const idWithPrefix: StringTypeDef = {
   name: "idWithPrefix",
   pluralName: "idsWithPrefix",
   summary:
-    `An id that identifies an object.  It is composed for 3 visible parts.
-     The first part is an optional capital letter that is used to denote the category
-     of an object, for example an 'N' for a normalised record, or a 'T' for a deep
-     tree structure.  This optional letter is used for internal ids.  The second part
-     is a sequence of 2 to 6 lowercase letters that denote the type of object.
-     The final section is a sequence of random characters that are based on the date
-     and time.  Ids generated is this manner will ordered chronilogically when ordered
+    `An id that identifies an object.  It is composed for 2 visible parts.
+     The first part is a sequence of 2 to 6 lowercase letters that denote the type of object.
+     The second part is a sequence of random characters that are based on the date
+     and time.  Ids generated is this manner will be ordered chronilogically when ordered
      alphabetically.`,
-  regex: "^[A-Z]?[a-z]{2,6}_[a-z0-9]{4,44}$",
+  regex: "^[a-z]{2,6}_[a-z0-9]{4,44}$",
   maximumLength: 50,
   validTestCases: [{
     value: "ex_234g23jh534hgk35jhkjhk",
   }, {
-    value: "Nex_test",
+    value: "ex_test",
   }, {
-    value: "Texampl_system",
+    value: "exampl_system",
   }],
   invalidTestCases: [{
     value: "ex234g23jh534hgk35jhkjhk",
   }, {
-    value: "XXcaps_ex234g23jh534hgk35jhkjhk",
+    value: "Xcaps_ex234g23jh534hgk35jhkjhk",
   }, {
     value: "toolong_234g23jh534hgk_35jhkjhk",
   }],
